798f7c9c8b2d32491319dd6e64455eac654874dd
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-21 10:32:00 +0300

    Added using of background thread.

--- app/src/main/java/com/example/lynnik/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/lynnik/photogallery/PhotoGalleryFragment.java
@@ -1,15 +1,21 @@
 package com.example.lynnik.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
+
+  private static final String TAG = "PhotoGalleryFragment";
 
   private RecyclerView mRecyclerView;
 
@@ -21,6 +27,7 @@ public class PhotoGalleryFragment extends Fragment {
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setRetainInstance(true);
+    new FetchItemTask().execute();
   }
 
   @Nullable
@@ -37,5 +44,21 @@ public class PhotoGalleryFragment extends Fragment {
     mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
     return v;
+  }
+
+  private class FetchItemTask extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+      try {
+        String result = new FlickrFetchr()
+            .getUrlString("https://www.bignerdranch.com");
+        Log.i(TAG, "Fetched contents of URL: " + result);
+      } catch (IOException ioe) {
+        Log.e(TAG, "Failed to fetch URL: ", ioe);
+      }
+
+      return null;
+    }
   }
 }
