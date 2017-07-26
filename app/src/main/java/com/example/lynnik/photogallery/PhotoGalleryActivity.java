f7026ee2f3f0a153d200b10cc84a226f6a334467
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-26 10:59:17 +0300

    Added notifications.

--- app/src/main/java/com/example/lynnik/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/example/lynnik/photogallery/PhotoGalleryActivity.java
@@ -1,8 +1,14 @@
 package com.example.lynnik.photogallery;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+  public static Intent newIntent(Context context) {
+    return new Intent(context, PhotoGalleryActivity.class);
+  }
 
   @Override
   protected Fragment createFragment() {
--- app/src/main/java/com/example/lynnik/photogallery/PollService.java
+++ app/src/main/java/com/example/lynnik/photogallery/PollService.java
@@ -2,12 +2,16 @@ package com.example.lynnik.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.List;
@@ -73,6 +77,23 @@ public class PollService extends IntentService {
       Log.i(TAG, "Got an old result: " + resultId);
     } else {
       Log.i(TAG, "Got a new result: " + resultId);
+
+      Resources resources = getResources();
+      Intent i = PhotoGalleryActivity.newIntent(this);
+      PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+      Notification notification = new NotificationCompat.Builder(this)
+          .setTicker(resources.getString(R.string.new_pictures_title))
+          .setSmallIcon(android.R.drawable.ic_menu_report_image)
+          .setContentTitle(resources.getString(R.string.new_pictures_title))
+          .setContentText(resources.getString(R.string.new_pictures_text))
+          .setContentIntent(pi)
+          .setAutoCancel(true)
+          .build();
+
+      NotificationManagerCompat notificationManager =
+          NotificationManagerCompat.from(this);
+      notificationManager.notify(0, notification);
     }
 
     QueryPreferences.setLastResultId(this, resultId);
