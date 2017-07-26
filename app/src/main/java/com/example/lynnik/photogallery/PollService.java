2a8c05c9f5e1fa0bb659b2388aaa68eda195fd58
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-26 09:57:32 +0300

    Added network access check.

--- app/src/main/java/com/example/lynnik/photogallery/PollService.java
+++ app/src/main/java/com/example/lynnik/photogallery/PollService.java
@@ -3,6 +3,7 @@ package com.example.lynnik.photogallery;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -20,6 +21,21 @@ public class PollService extends IntentService {
 
   @Override
   protected void onHandleIntent(@Nullable Intent intent) {
+    if (!isNetworkAvailableAndConnected()) {
+      return;
+    }
+
     Log.i(TAG, "Received an intent: " + intent);
+  }
+
+  private boolean isNetworkAvailableAndConnected() {
+    ConnectivityManager cm = (ConnectivityManager)
+        getSystemService(CONNECTIVITY_SERVICE);
+
+    boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+    boolean isNetworkConnected = isNetworkAvailable &&
+        cm.getActiveNetworkInfo().isConnected();
+
+    return isNetworkConnected;
   }
 }
