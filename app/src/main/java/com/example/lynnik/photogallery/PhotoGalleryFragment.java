c31cd06543afeb3e8bf81fd96d889ed757c8defa
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-26 10:43:21 +0300

    Added signal control.

--- app/src/main/java/com/example/lynnik/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/lynnik/photogallery/PhotoGalleryFragment.java
@@ -42,8 +42,6 @@ public class PhotoGalleryFragment extends Fragment {
     setHasOptionsMenu(true);
     updateItems();
 
-    PollService.setServiceAlarm(getActivity(), true);
-
     Handler responseHandler = new Handler();
     mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
     mThumbnailDownloader.setThumbnailDownloadListener(
@@ -121,6 +119,12 @@ public class PhotoGalleryFragment extends Fragment {
         searchView.setQuery(query, false);
       }
     });
+
+    MenuItem toggleItem = menu.findItem(R.id.menu_item_toggle_polling);
+    if (PollService.isServiceAlarmOn(getActivity()))
+      toggleItem.setTitle(R.string.stop_polling);
+    else
+      toggleItem.setTitle(R.string.start_polling);
   }
 
   @Override
@@ -129,6 +133,11 @@ public class PhotoGalleryFragment extends Fragment {
       case R.id.menu_item_clear:
         QueryPreferences.setStoredQuery(getActivity(), null);
         updateItems();
+        return true;
+      case R.id.menu_item_toggle_polling:
+        boolean shouldStartAlarm = !PollService.isServiceAlarmOn(getActivity());
+        PollService.setServiceAlarm(getActivity(), shouldStartAlarm);
+        getActivity().invalidateOptionsMenu();
         return true;
       default:
         return super.onOptionsItemSelected(item);
