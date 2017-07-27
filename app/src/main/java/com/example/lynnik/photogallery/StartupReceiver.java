9474076a08eb7cfbff15dc3138b2eff8a57613cb
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-27 12:18:38 +0300

    Broadcast receiver added.

---
+++ app/src/main/java/com/example/lynnik/photogallery/StartupReceiver.java
@@ -0,0 +1,16 @@
+package com.example.lynnik.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class StartupReceiver extends BroadcastReceiver {
+
+  public static final String TAG = "StartupReceiver";
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+  }
+}
